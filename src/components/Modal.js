7c339bedf337f513ee069d8228f4a89eb329c016
Abdullah khan <akhan283785@gmail.com>
2024-11-19 03:40:33 +0530

    bhai test case acche bnao

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = () => {
   return (
     <div className="child">
-      <h2>Modal Content</h2>
+      <h3>Modal Content</h3>
       <p>This is the Modal Content</p>
     </div>
   );
